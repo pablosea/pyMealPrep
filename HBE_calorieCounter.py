bdb55cd0d25532bdbe5d593aae2e206a5d12d455
Pablosea <cipollapauljr@gmail.com>
2023-01-21 12:32:03 -0500

    Added comments and updated readme

--- HBE_calorieCounter.py
+++ HBE_calorieCounter.py
@@ -1,6 +1,6 @@
-# Harris-Benedict equation
+#Function fo Harris-Benedict equation
 def calculate_calories(age, weight, height, gender, activity_level):
-  # Calculate BMR
+  #Calculate BMR
   height_feet, height_inches = height
   BMR = 10 * weight + 6.25 * (height_feet * 12 + height_inches) - 5 * age
   if gender == "male":
@@ -8,7 +8,7 @@ def calculate_calories(age, weight, height, gender, activity_level):
   else:
     BMR -= 161
 
-  # Adjust BMR for activity level
+  #Adjust BMR for activity level
   if activity_level == "sedentary":
     calories = BMR * 1.2
   elif activity_level == "lightly active":
@@ -22,21 +22,32 @@ def calculate_calories(age, weight, height, gender, activity_level):
 
   return calories
 
+#Convert to grams function
 def convert_macros_to_grams(calories, protein_percent, carb_percent, fat_percent):
     protein_grams = calories * protein_percent / 4
     carb_grams = calories * carb_percent / 4
     fat_grams = calories * fat_percent / 9
     return protein_grams, carb_grams, fat_grams
 
-# Calculate daily caloric needs for a person
+#Hard code your variables lol
 age = 0
 weight = 0
 height = (0,0) #(feet, inches)
 gender = "male"
 activity_level = "very active"
 cal_imbalance = 0 #positve number for caloric surplus; negaitve number for caloric defict 
+
+#Edit these percentages to fit your desired workout/ nutrition goal 
+#(I'm carb heavy for runing long distance/ I'll change to protien heavy for weightlifting)
+protein_percent = 0.35
+carb_percent = 0.50
+fat_percent = 0.25
+
+if protein_percent + carbon_percent + fat_percent != 1:
+  print("Your percentages do not equal to 100%")
+
 calories = calculate_calories(age, weight, height, gender, activity_level)
-protein_grams, carb_grams, fat_grams = convert_macros_to_grams(calories+cal_imbalance, 0.35, 0.5, 0.25)
+protein_grams, carb_grams, fat_grams = convert_macros_to_grams(calories+cal_imbalance, 0.35, 0.5, 0.25) #
 
 print(" ")
 print(" ")
--- readme.md
+++ readme.md
@@ -2,4 +2,8 @@
 
 This program is designed to calculate your caloric intake and convert it to grams of protein, fats, and carbs based on the macro percentages you choose.
 
-Its just way easier to use this than convert everyhting to grams by hand.
+Its just way easier to use this than converting everything to grams by hand for meal prep.
+
+Edit the file for your personal stats.
+
+Always consult physcians for health stuff.
